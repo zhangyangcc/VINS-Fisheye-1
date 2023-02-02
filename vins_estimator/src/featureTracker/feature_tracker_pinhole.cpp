77d7f417f3f73816a5f822ffbd8e24320f3f6f0e
zhangyang <958827244@qq.com>
2023-02-02 15:24:13 +0800

    fix the bug 'no reference to addPoints'

--- vins_estimator/src/featureTracker/feature_tracker_pinhole.cpp
+++ vins_estimator/src/featureTracker/feature_tracker_pinhole.cpp
@@ -195,4 +195,154 @@ void PinholeFeatureTracker<CvMat>::setPrediction(const map<int, Eigen::Vector3d>
             predict_pts.push_back(prev_pts[i]);
     }
 }
+
+FeatureFrame PinholeFeatureTrackerCPU::trackImage(double _cur_time, cv::InputArray _img, 
+        cv::InputArray _img1)
+{
+    static double detected_time_sum = 0;
+    static double ft_time_sum = 0;
+    static int count = 0;
+    count += 1;
+
+    TicToc t_r;
+    cur_time = _cur_time;
+    cur_img = _img.getMat();
+    cv::Mat right_img = _img1.getMat();;
+
+    height = cur_img.rows;
+    width = cur_img.cols;
+    cur_pts.clear();
+    TicToc t_ft;
+    cur_pts = opticalflow_track(cur_img, prev_img, prev_pts, ids, track_cnt, removed_pts);
+    ft_time_sum += t_ft.toc();
+
+    TicToc t_d;
+    detectPoints(cur_img, cv::Mat(), n_pts, cur_pts, MAX_CNT);
+    detected_time_sum = detected_time_sum + t_d.toc();
+
+    addPoints();
+
+    cur_un_pts = undistortedPts(cur_pts, m_camera[0]);
+    pts_velocity = ptsVelocity(ids, cur_un_pts, cur_un_pts_map, prev_un_pts_map);
+
+    if(!_img1.empty() && stereo_cam)
+    {
+        t_ft.tic();
+        ids_right = ids;
+        std::vector<cv::Point2f> right_side_init_pts = cur_pts;
+        cur_right_pts = opticalflow_track(right_img, cur_img, right_side_init_pts, ids_right, track_right_cnt, removed_pts);
+        cur_un_right_pts = undistortedPts(cur_right_pts, m_camera[1]);
+        right_pts_velocity = ptsVelocity(ids_right, cur_un_right_pts, cur_un_right_pts_map, prev_un_right_pts_map);
+        ft_time_sum += t_ft.toc();
+    }
+
+    if(SHOW_TRACK)
+    {
+        drawTrack(cur_img, right_img, ids, cur_pts, cur_right_pts, prevLeftPtsMap);
+    }
+
+    prev_img = cur_img;
+    prev_pts = cur_pts;
+    prev_un_pts = cur_un_pts;
+    prev_un_pts_map = cur_un_pts_map;
+    prev_un_right_pts_map = cur_un_right_pts_map;
+
+    prev_time = cur_time;
+    hasPrediction = false;
+
+    prevLeftPtsMap.clear();
+    for(size_t i = 0; i < cur_pts.size(); i++)
+        prevLeftPtsMap[ids[i]] = cur_pts[i];
+
+    FeatureFrame featureFrame;
+    BaseFeatureTracker::setup_feature_frame(featureFrame, ids, cur_pts, cur_un_pts, pts_velocity, 0);   
+    BaseFeatureTracker::setup_feature_frame(featureFrame, ids_right, cur_right_pts, cur_un_right_pts, right_pts_velocity, 1);   
+
+    printf("Img: %d: trackImage: %3.1fms; PT NUM: %ld, STEREO: %ld; Avg: GFTT %3.1fms LKFlow %3.1fms\n", 
+        count,
+        t_r.toc(), 
+        cur_pts.size(),
+        cur_right_pts.size(),
+        detected_time_sum/count, 
+        ft_time_sum/count);
+    return featureFrame;
+}
+
+FeatureFrame PinholeFeatureTrackerCuda::trackImage(double _cur_time, cv::InputArray _img, 
+        cv::InputArray _img1)
+{
+#ifndef WITHOUT_CUDA
+    static double detected_time_sum = 0;
+    static double ft_time_sum = 0;
+    static int count = 0;
+    count += 1;
+
+    TicToc t_r;
+    cur_time = _cur_time;
+    cv::Mat rightImg;
+    cv::cuda::GpuMat cur_gpu_img = cv::cuda::GpuMat(_img);
+    cv::cuda::GpuMat right_gpu_img = cv::cuda::GpuMat(_img1);
+
+    height = cur_gpu_img.rows;
+    width = cur_gpu_img.cols;
+
+    cur_pts.clear();
+    TicToc t_ft;
+    cur_pts = opticalflow_track(cur_gpu_img, prev_pyr, prev_pts, ids, track_cnt, removed_pts, false);
+    ft_time_sum += t_ft.toc();
+
+    TicToc t_d;
+    detectPoints(cur_gpu_img, n_pts, cur_pts, MAX_CNT);
+    detected_time_sum = detected_time_sum + t_d.toc();
+
+    addPoints();
+
+    cur_un_pts = undistortedPts(cur_pts, m_camera[0]);
+    pts_velocity = ptsVelocity(ids, cur_un_pts, cur_un_pts_map, prev_un_pts_map);
+
+    if(!_img1.empty() && stereo_cam)
+    {
+        t_ft.tic();
+        ids_right = ids;
+        std::vector<cv::Point2f> right_side_init_pts = cur_pts;
+        cur_right_pts = opticalflow_track(right_gpu_img, prev_pyr, right_side_init_pts, ids_right, track_right_cnt, removed_pts, true);
+        cur_un_right_pts = undistortedPts(cur_right_pts, m_camera[1]);
+        right_pts_velocity = ptsVelocity(ids_right, cur_un_right_pts, cur_un_right_pts_map, prev_un_right_pts_map);
+        ft_time_sum += t_ft.toc();
+    }
+
+    if(SHOW_TRACK)
+    {
+        cur_gpu_img.download(cur_img);
+        right_gpu_img.download(rightImg);
+        drawTrack(cur_img, rightImg, ids, cur_pts, cur_right_pts, prevLeftPtsMap);
+    }
+
+    prev_gpu_img = cur_gpu_img;
+    prev_pts = cur_pts;
+    prev_un_pts = cur_un_pts;
+    prev_un_pts_map = cur_un_pts_map;
+    prev_un_right_pts_map = cur_un_right_pts_map;
+
+    prev_time = cur_time;
+    hasPrediction = false;
+
+    prevLeftPtsMap.clear();
+    for(size_t i = 0; i < cur_pts.size(); i++)
+        prevLeftPtsMap[ids[i]] = cur_pts[i];
+
+    FeatureFrame featureFrame;
+    BaseFeatureTracker::setup_feature_frame(featureFrame, ids, cur_pts, cur_un_pts, pts_velocity, 0);   
+    BaseFeatureTracker::setup_feature_frame(featureFrame, ids_right, cur_right_pts, cur_un_right_pts, right_pts_velocity, 1);   
+
+    printf("Img: %d: trackImage: %3.1fms; PT NUM: %ld, STEREO: %ld; Avg: GFTT %3.1fms LKFlow %3.1fms\n", 
+        count,
+        t_r.toc(), 
+        cur_pts.size(),
+        cur_right_pts.size(),
+        detected_time_sum/count, 
+        ft_time_sum/count);
+    return featureFrame;
+#endif
+}
 }
